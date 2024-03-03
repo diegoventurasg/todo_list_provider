1f278342e7bcc3593632f3edefa53c81b0bf8e40
Diego Ventura <diegoventurasg@gmail.com>
2024-03-03 11:33:31 -0300

    move o módulo auth para o diretório correto

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list_provider/app/core/database/sqlite_adm_connection.dart';
 import 'package:todo_list_provider/app/core/ui/todo_list_ui_config.dart';
-import 'package:todo_list_provider/app/modules/splash/auth/auth_module.dart';
+import 'package:todo_list_provider/app/modules/auth/auth_module.dart';
 import 'package:todo_list_provider/app/modules/splash/splash_page.dart';
 
 class AppWidget extends StatefulWidget {
--- lib/app/modules/auth/auth_module.dart
+++ lib/app/modules/auth/auth_module.dart
@@ -1,7 +1,7 @@
 import 'package:provider/provider.dart';
 import 'package:todo_list_provider/app/core/modules/todo_list_module.dart';
-import 'package:todo_list_provider/app/modules/splash/auth/login/login_controller.dart';
-import 'package:todo_list_provider/app/modules/splash/auth/login/login_page.dart';
+import 'package:todo_list_provider/app/modules/auth/login/login_controller.dart';
+import 'package:todo_list_provider/app/modules/auth/login/login_page.dart';
 
 class AuthModule extends TodoListModule {
   AuthModule()
